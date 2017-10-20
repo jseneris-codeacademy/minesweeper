03cf3c9d7169bb4d2a61d17bd1d5de3f468f53d0
jseneris <johnseneris@gmail.com>
2017-10-20 14:27:24 -0400

    Day 10

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,46 @@
-const printBoard = board => {
-  console.log('Current Board:');
-  console.log(board[0].join('|'));
-  console.log(board[1].join('|'));
-  console.log(board[2].join('|'));
-};
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+  for (let rowCount = 0; rowCount < numberOfRows; rowCount++){
+    const row = [];
+    for (let colCount = 0; colCount < numberOfColumns; colCount++){
+      row.push(' ');
+    }
+    board.push(row);
+  }
+
+  return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+
+  for (let rowCount = 0; rowCount < numberOfRows; rowCount++){
+    const row = [];
+    for (let colCount = 0; colCount < numberOfColumns; colCount++){
+      row.push(null);
+    }
+    board.push(row);
+  }
+
+  let numberofBombsPlaced = 0;
+  while (numberofBombsPlaced < numberOfBombs){
+    let randomRowIndex = Math.floor(Math.random()*numberOfRows);
+    let randomColIndex = Math.floor(Math.random()*numberOfColumns);
+    if (board[randomRowIndex][randomColIndex] === null){
+      board[randomRowIndex][randomColIndex] = 'B';
+      numberofBombsPlaced++;
+    }
+  }
+
+  return board;
+};
+
+const printBoard = board => {
+  console.log(board.map(row => row.join('|')).join('\n'));
+};
+
+console.log('Player Board: ')
+printBoard(generatePlayerBoard(3,4));
+console.log('Bomb Board: ')
+printBoard(generateBombBoard(3,4,5));
